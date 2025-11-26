60105d021ef51eb3c63570d03e1bdb6880b36da5
Piyush Sontakke <piyushsontakke@Piyushs-MacBook-Air.local>
2025-11-27 00:36:48 +0530

    Image

--- components/login-form.tsx
+++ components/login-form.tsx
@@ -23,7 +23,7 @@ export function LoginForm({
                             <div className="flex flex-col items-center gap-2 text-center">
                                 <h1 className="text-2xl font-bold">Welcome back</h1>
                                 <p className="text-muted-foreground text-balance">
-                                    Login to your Acme Inc account
+                                    Login to your Vibe Trading account
                                 </p>
                             </div>
                             <Field>
@@ -89,7 +89,7 @@ export function LoginForm({
                     </form>
                     <div className="bg-muted relative hidden md:block">
                         <img
-                            src="/placeholder.svg"
+                            src="/bull.jpg"
                             alt="Image"
                             className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
                         />
