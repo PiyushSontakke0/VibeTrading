82bb0fdde18b2fb2abf3501b26ab9b7001fe59cb
Piyush Sontakke <piyushsontakke@Piyushs-MacBook-Air.local>
2025-11-27 00:52:57 +0530

    Added sign up form

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,10 +1,10 @@
-import { SignUpForm } from "@/components/signup-form"
+import { LoginForm } from "@/components/login-form"
 
-export default function () {
+export default function LoginPage() {
     return (
         <div className="bg-muted flex min-h-svh flex-col items-center justify-center p-6 md:p-10">
             <div className="w-full max-w-sm md:max-w-4xl">
-                <SignUpForm />
+                <LoginForm />
             </div>
         </div>
     )
